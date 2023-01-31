66ed90fbf8369a4da236418e3aa70e5ceb906a2e
Tom Ko <yungsu2391@gmail.com>
2023-02-01 07:50:37 +0900

    폴더 구조 최적화

--- src/bin/www.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require("../app")
-const PORT = 3001;
-
-app.listen(PORT, async () => {
-    console.log("서버 가동");
-});
--- src/routes/home/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-const {home, login} = require('./home.controller')
-
-router.get("/", home)
-router.get("/login",login)
-
-module.exports = router;
