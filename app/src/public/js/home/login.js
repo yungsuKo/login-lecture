17cd6d2744385a7aaf5b65860b6d57f557830940
Tom Ko <yungsu2391@gmail.com>
2023-02-01 08:14:33 +0900

    돔을 이용하여 html 요소 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,17 @@
 "use strict";
-console.log("Hellooooo");
-console.log("toutoutou")
+// dom -> document object model
+
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener('click',login);
+
+function login(event){
+    event.preventDefault();
+    const req = {
+        id: id.value,
+        password : password.value,
+    }
+    console.log(req)
+};
