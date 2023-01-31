35a448315a083b34d64f2f7e68ad2b0feaab8ee7
Tom Ko <yungsu2391@gmail.com>
2023-02-01 07:58:22 +0900

    프런트 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', "./src/views");
 app.set('view engine', "ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home);
 
